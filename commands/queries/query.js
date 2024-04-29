f07708f34e830e38064a72caa12cc7d970b5b1b6
kazzrom <roma.kazansew.04@mail.ru>
2024-04-29 14:59:38 +0500

    Маленькие изменения

--- commands/queries/query.js
+++
@@ -1,9 +0,0 @@
-import initModels from "../../models/initModels.js";
-
-const { Student, PersonalData, FamilyMember } = initModels();
-
-const student = await Student.findByPk(1, {
-  include: [PersonalData, FamilyMember],
-});
-
-console.log(student.toJSON());
